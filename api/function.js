3b208e21d4dfac09e10c78cc0801121f19bc6961
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-09-24 14:23:48 +0200

    lint

--- api/function.js
+++ api/function.js
@@ -1,18 +1,20 @@
-const config = require(__dirname + '/apiConfig.js')
+const path = require('path')
+const config = path.join(__dirname, '/apiConfig.js')
+
 const justifyText = text => {
   let formatedText = []
   let charCounter = 0
   let line = []
   let i = 0
 
-  const addSpace = str => {                            // add space until str.length === maxcharbyline
+  const addSpace = str => { // add space until str.length === maxcharbyline
     let spaceNeeded = config.maxCharByLine - str.length
-    str = str.split(' ')                               // array of words
-    const lastWord = str.pop()                         // remove last word, so i don't add space after it
-    str = str.map(word => word += ' ')                 // add space after every word
+    str = str.split(' ') // array of words
+    const lastWord = str.pop() // remove last word, so i don't add space after it
+    str = str.map(word => word += ' ') // add space after every word
 
-    while (spaceNeeded > 0) {                          // add space equally between every word
-      for(let i = 0; str[i]; i++) {
+    while (spaceNeeded > 0) { // add space equally between every word
+      for (let i = 0; str[i]; i++) {
         if (spaceNeeded > 0) {
           str[i] += ' '
           spaceNeeded--
@@ -20,21 +22,21 @@ const justifyText = text => {
       }
     }
 
-    str.push(lastWord)                                 // add the last word
-    
+    str.push(lastWord) // add the last word
+
     return str.join('')
   }
 
   const longWord = str => {
-    const spaceLeftLine = line[0] ? 
-    config.maxCharByLine - line[0].length :
-    config.maxCharByLine
+    const spaceLeftLine = line[0]
+      ? config.maxCharByLine - line[0].length
+      : config.maxCharByLine
 
     const charCanFit = str.slice(0, spaceLeftLine)
-    str = str.slice(spaceLeftLine)                   // cut the word 
-    line.push(charCanFit.join(''))                   // and put first part in the actual line
-    formatedText[i] = line.join('')                  // save the actual line
-    line = []                                        // new line 
+    str = str.slice(spaceLeftLine) // cut the word
+    line.push(charCanFit.join('')) // and put first part in the actual line
+    formatedText[i] = line.join('') // save the actual line
+    line = [] // new line
     i++
 
     while (str.length > config.maxCharByLine) {
@@ -44,46 +46,45 @@ const justifyText = text => {
       line = []
       i++
     }
-    
+
     line.push(str.join(''))
 
-    charCounter = line.join('').length               // return the size of the line
+    charCounter = line.join('').length // return the size of the line
   }
-  
+
   text.trim()
-    .split('\n')                                       // save the format of the text
+    .split('\n') // save the format of the text
     .forEach((paragraph) => {
       paragraph.trim()
-        .split(' ')                                    // split on every word
-        .map((word, index) => {                        // transform word into array of chars and add a space before words
+        .split(' ') // split on every word
+        .map((word, index) => { // transform word into array of chars and add a space before words
           word = word.split('')
-          if (index > 0) word.unshift(' ')             // don't add space if it's the first word of a paragraph
+          if (index > 0) word.unshift(' ') // don't add space if it's the first word of a paragraph
           return word
         })
         .forEach(word => {
           if (word.length > 80) {
             longWord(word)
           } else {
-              charCounter += word.length                   // size of the actual line + the actual word 
+            charCounter += word.length // size of the actual line + the actual word
 
-            if(charCounter <= config.maxCharByLine) {
+            if (charCounter <= config.maxCharByLine) {
               line.push(word.join(''))
-
-            } else if (charCounter > config.maxCharByLine) {    // if the word can't fit in the actual line 
-              formatedText[i] = line.join('')            // save the actual line
-              formatedText[i] = addSpace(formatedText[i])// and justify it
-              line = []                                  // new line 
-              line.push(word.join('').trim())            // remove space, put the word on a new line
-              charCounter = word.length                  // and set the counter with the actual word length
+            } else if (charCounter > config.maxCharByLine) { // if the word can't fit in the actual line
+              formatedText[i] = line.join('') // save the actual line
+              formatedText[i] = addSpace(formatedText[i]) // and justify it
+              line = [] // new line
+              line.push(word.join('').trim()) // remove space, put the word on a new line
+              charCounter = word.length // and set the counter with the actual word length
               i++
             }
           }
-          charCounter = line.join('').length           // return the size of the line
+          charCounter = line.join('').length // return the size of the line
         })
-        
-      formatedText[formatedText.length - 1] !== '\n' ? // add \n if it's a new paragraph 
-      formatedText.push(line.join('')) :
-      formatedText.push(line.join(''), '\n')
+
+      formatedText[formatedText.length - 1] !== '\n' // add \n if it's a new paragraph
+        ? formatedText.push(line.join(''))
+        : formatedText.push(line.join(''), '\n')
 
       charCounter = 0
       line = []
@@ -94,6 +95,5 @@ const justifyText = text => {
 }
 
 module.exports = {
-  justifyText,
+  justifyText
 }
-
