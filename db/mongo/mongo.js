f41baf9ea25bfafe3558f106b98123509f5cc0c6
Mehdi Chtira <mehdi.chtira@gmail.com>
2018-09-24 17:23:46 +0200

    readme correction, rename folder

--- db/mongo/mongo.js
+++ db/mongo/mongo.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const mongoose = require('mongoose')
 const config = require(path.join(__dirname, '/../../api/apiConfig.js'))
-const UserModel = require(path.join(__dirname, '/model/UserModel.js'))
-const mongoUrl = process.env.mongoDb || 'mongodb://anyway:anyway2205@ds113003.mlab.com:13003/overkilldb'
+const UserModel = require(path.join(__dirname, '/models/UserModel.js'))
+const mongoUrl = process.env.mongoDb
 const d = new Date()
 const date = d.toJSON().slice(0, 10)
 
@@ -21,9 +21,11 @@ const updateUser = async ({ email, numberOfWord }) => {
   const user = await UserModel.findOne({ email })
   if (!user) {
     if (numberOfWord) {
-      try { await new UserModel({ email, totalWord: numberOfWord, date }).save() } catch (err) { throw err }
+      try { await new UserModel({ email, totalWord: numberOfWord, date }).save() } 
+      catch (err) { throw err }
     } else {
-      try { await new UserModel({ email, totalWord: 0, date }).save() } catch (err) { throw err }
+      try { await new UserModel({ email, totalWord: 0, date }).save() } 
+      catch (err) { throw err }
     }
 
     return ('new user created')
@@ -32,9 +34,11 @@ const updateUser = async ({ email, numberOfWord }) => {
       const query = { email: email }
       if (date === user.date) {
         user.totalWord += numberOfWord
-        try { await UserModel.findOneAndUpdate(query, user) } catch (err) { throw err }
+        try { await UserModel.findOneAndUpdate(query, user) } 
+        catch (err) { throw err }
       } else {
-        try { await UserModel.findOneAndUpdate(query, { email, totalWord: numberOfWord, date }) } catch (err) { throw err }
+        try { await UserModel.findOneAndUpdate(query, { email, totalWord: numberOfWord, date }) } 
+        catch (err) { throw err }
       }
 
       return ('user updated')
@@ -48,7 +52,8 @@ const canUseApi = async (email, numberOfWord) => {
   const isAllowed = user.totalWord <= config.maxWordPerDay
   if (!isAllowed) {
     user.totalWord -= numberOfWord
-    try { await UserModel.findOneAndUpdate(query, user) } catch (err) { throw err }
+    try { await UserModel.findOneAndUpdate(query, user) } 
+    catch (err) { throw err }
   }
 
   return isAllowed
